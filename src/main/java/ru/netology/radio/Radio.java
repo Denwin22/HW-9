36b8e27278e3b6ab75c95ebb6e625b070b002170
Denwin <biscooter@mail.ru>
2023-09-11 11:17:12 +0600

    update

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,88 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int maxRadioWave;
+    private int minRadioWave;
+    private int currentRadioWave = minRadioWave;
+    private int maxSoundLevel = 100;
+    private int minSoundLevel = 0;
+    private int currentSoundLevel = minSoundLevel;
+
+    public Radio() {
+        maxRadioWave = 9;
+        minRadioWave = 0;
+    }
+
+    public Radio(int stationCount) {
+        maxRadioWave = stationCount - 1;
+        if (stationCount < 0) {
+            maxRadioWave = currentRadioWave;
+        }
+    }
+
+    public int getMaxRadioWave() {
+        return maxRadioWave;
+    }
+
+    public int getMinRadioWave() {
+        return minRadioWave;
+    }
+
+    public int getCurrentRadioWave() {
+        return currentRadioWave;
+    }
+
+    public int getCurrentSoundLevel() {
+        return currentSoundLevel;
+    }
+
+    public void setCurrentRadioWave(int newCurrentRadioWave) {
+        if (newCurrentRadioWave < minRadioWave) {
+            currentRadioWave = minRadioWave;
+            return;
+        }
+        if (newCurrentRadioWave > maxRadioWave) {
+            currentRadioWave = minRadioWave;
+            return;
+        }
+        currentRadioWave = newCurrentRadioWave;
+    }
+
+    public void setNextRadioWave() {
+        if (currentRadioWave == maxRadioWave) {
+            currentRadioWave = minRadioWave;
+        } else {
+            currentRadioWave++;
+        }
+    }
+
+    public void setPrevRadioWave() {
+        if (currentRadioWave == minRadioWave) {
+            currentRadioWave = maxRadioWave;
+        } else {
+            currentRadioWave--;
+        }
+    }
+
+    public void increaseSoundLevel() {
+        if (currentSoundLevel < maxSoundLevel) {
+            currentSoundLevel = currentSoundLevel + 1;
+        }
+    }
+
+    public void decreaseSoundLevel() {
+        if (currentSoundLevel > minSoundLevel) {
+            currentSoundLevel = currentSoundLevel - 1;
+        }
+    }
+
+    public void setCurrentSoundLevel(int newCurrentSoundLevel) {
+        if (newCurrentSoundLevel > maxSoundLevel) {
+            return;
+        }
+        if (newCurrentSoundLevel < minSoundLevel) {
+            return;
+        }
+        currentSoundLevel = newCurrentSoundLevel;
+    }
+}
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,113 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.security.PublicKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RadioTest {
+
+    @Test
+    public void inputRadioWave() {
+        Radio radio = new Radio(20);
+        Assertions.assertEquals(19, radio.getMaxRadioWave());
+        Assertions.assertEquals(0, radio.getMinRadioWave());
+        Assertions.assertEquals(0, radio.getCurrentRadioWave());
+    }
+    @Test
+    public void inputNegativeRadioWave() {
+        Radio radio = new Radio(-20);
+        Assertions.assertEquals(0, radio.getMaxRadioWave());
+        Assertions.assertEquals(0, radio.getMinRadioWave());
+        Assertions.assertEquals(0, radio.getCurrentRadioWave());
+    }
+    @Test
+    public void nextRadioWaveIfMax() {
+        Radio radio = new Radio(11);
+        radio.setCurrentRadioWave(10);
+        radio.setNextRadioWave();
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioWave();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void nextRadioWaveIfMin() {
+        Radio radio = new Radio(33);
+        radio.setCurrentRadioWave(0);
+        radio.setNextRadioWave();
+
+        int expected = 1;
+        int actual = radio.getCurrentRadioWave();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void prevRadioWaveIfMin() {
+        Radio radio = new Radio(51);
+        radio.setCurrentRadioWave(0);
+        radio.setPrevRadioWave();
+
+        int expected = 50;
+        int actual = radio.getCurrentRadioWave();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void prevRadioWaveIfMax() {
+        Radio radio = new Radio(40);
+        radio.setCurrentRadioWave(39);
+        radio.setPrevRadioWave();
+
+        int expected = 38;
+        int actual = radio.getCurrentRadioWave();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void increaseSoundLevelIfMax() {
+        Radio radio = new Radio();
+        radio.setCurrentSoundLevel(100);
+        radio.increaseSoundLevel();
+
+        int expected = 100;
+        int actual = radio.getCurrentSoundLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseSoundLevelIfMin() {
+        Radio radio = new Radio();
+        radio.setCurrentSoundLevel(0);
+        radio.increaseSoundLevel();
+
+        int expected = 1;
+        int actual = radio.getCurrentSoundLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseSoundLevelIfMin() {
+        Radio radio = new Radio();
+        radio.setCurrentSoundLevel(0);
+        radio.decreaseSoundLevel();
+
+        int expected = 0;
+        int actual = radio.getCurrentSoundLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseSoundLevelIfMax() {
+        Radio radio = new Radio();
+        radio.setCurrentSoundLevel(100);
+        radio.decreaseSoundLevel();
+
+        int expected = 99;
+        int actual = radio.getCurrentSoundLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
